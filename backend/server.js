44679abc113c9bc2fde1adf42d84d9e0421d3173
shadow855 <raiyanmushtaque@gmail.com>
2024-06-13 06:31:06 +0530

    updated server.js to add frontend link of render

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://dept-finance.onrender.com"]
+    origin: ["http://localhost:3000", "https://task-management-web-app-fzjf.onrender.com"]
 }));
 
 app.use('/api/user', userRoutes);
